b5464250876997528f3c59ecf4996e5563abfc75
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-06-06 14:08:40 +0400

    create new branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
 
-import { addCount, getCount } from './features/counter/counterSlice'
-import { getAllPosts, getPosts } from './features/posts/postsSlice';
+import './App.css'
 
 export default function App() {
-  const [currentId, setCurrentId] = useState(true)
-
-  useEffect(() => {
-    dispatch(getPosts())
-
-  }, [])
-
-  const handleClick = () => {
-    dispatch(addCount('6'))
-  }
-
   return (
-    <div>
-      <h1 >{count}</h1>
-      <button onClick={handleClick}>add count</button>
-      <footer>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis sed labore dicta, sunt enim veritatis neque officia nulla quisquam eligendi.</p>
-      </footer>
+    <div className='App'>
+      <h1>New Branch</h1>
     </div>
   )
 }
